3e8c74f673fb7e90317caf967494aa8778e42eba
Le Anh Tuan <tuanla1@ssi.com.vn>
2020-10-18 12:22:29 +0700

    add example

--- flutter_expand_view/example/lib/main.dart
+++ flutter_expand_view/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expand_view/expand_child_widget.dart';
 
 
 void main() {
@@ -26,60 +27,55 @@ class TestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-        width: MediaQuery.of(context).size.width - 40,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 8),
-          child: Column(
-            children: [
-              Container(
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Flutter: Expansion/Collapse view', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
+      body: Column(
+        children: [
+          SizedBox(height: 100,),
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+            width: MediaQuery.of(context).size.width - 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: Column(
+                children: [
+                  Container(
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Flutter: Expansion/Collapse view', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
 
-                  ],
-                ),
+                      ],
+                    ),
+                  ),
+                  ExpandChildWidget(
+                    arrowPadding: const EdgeInsets.only(bottom: 0),
+                    expand: false,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width  - 50,
+                                height: 100,
+                                color: Colors.blueAccent,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-//              ExpandChildWidget(
-//                arrowPadding: const EdgeInsets.only(bottom: 0),
-//                expand: false,
-//                child: Column(
-//                  children: <Widget>[
-//                    Container(
-//                      child: Row(
-//                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                        children: [
-//                          _buildComponentInfoUser(
-//                              context,
-//                              'common_pl'.tr(),
-//                              const Text('123'),
-//                              'assets/images/icons/Ic_14_PL@1.5x.svg'),
-//                          Container(
-//                            width: 1,
-//                            height: 30,
-//                            color: Colors.grey,
-//                          ),
-//                          _buildComponentInfoUser(
-//                              context,
-//                              'common_risk'.tr(),
-//                              const Text('123'),
-//                              'assets/images/icons/Ic_14_white_risk@1.5x.svg'),
-//                        ],
-//                      ),
-//                    )
-//                  ],
-//                ),
-//              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
