66ff6c030cd64e681f7e35db9148045116cccc8b
Le Anh Tuan <tuanla1@ssi.com.vn>
2021-01-21 08:54:39 +0700

    update change log and export.

--- flutter_expand_view/lib/flutter_expand_view.dart
+++ flutter_expand_view/lib/flutter_expand_view.dart
@@ -2,6 +2,7 @@ library flutter_expand_view;
 
 export 'expand_child_widget.dart';
 export 'arrow_widget.dart';
+export 'expanded_collapse_right_widget.dart';
 
 /// A Calculator.
 class Calculator {
